be7fc819123d5068040e29024f131a6d4947be04
Ugochukwu <emdeefx@gmail.com>
2025-07-24 20:31:39 +0100

    added dist directory to root

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const morgan = require('morgan')
 const app = express()
 app.use(express.json())
+app.use(cors())
+app.use(express.static('dist'))
 morgan.token('body', function (request) {return JSON.stringify(request.body)})
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
@@ -94,7 +97,7 @@ app.post('/api/persons', (request, response) => {
   }
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`)
 })
